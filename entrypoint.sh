d3e175d7c133237a5a812e956d8202afb24d4297
Joseph Ralph <joseph@pmconnect.co.uk>
2018-07-04 10:20:18 +0100

    Adding php memory limit settings.

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,6 +29,11 @@ if [ ! -z ${PHP_UPLOAD_SIZE_MAX_MB+x} ]; then
   sed -ri "s#client_max_body_size [0-9]+m;#client_max_body_size ${PHP_UPLOAD_SIZE_MAX_MB}m;#g" /etc/nginx/sites-available/site.conf
 fi
 
+if [ ! -z ${PHP_MEMORY_LIMIT+x} ]; then
+  echo "Setting php memory limit..."
+  sed -ri "s#memory_limit = [0-9]+M#memory_limit = ${PHP_MEMORY_LIMIT}M#g" /usr/local/etc/php/php.ini
+fi
+
 if [ ! -z ${DEPLOYMENT_SCRIPT_PATH+x} ]; then
   if [ -f "$DEPLOYMENT_SCRIPT_PATH" ]; then
     echo "Making deployment up script executable..."
